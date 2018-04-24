89fe2f63e5005655c1e14c8e195066b7ae6c1609
yangguodonglop <362960082@qq.com>
2018-04-24 16:40:18 +0800

    2018:16:40

--- js/base_data.js
+++ js/base_data.js
@@ -3,6 +3,8 @@ URLS= 'http://mapi.xinlv123.com/xltx/mobile';
 SCORDERURL = 'http://mapi.xinlv123.com/xltx/mobile';
 goods_details ='http://mapi.xinlv123.com/xltx/mobile/goods/details';
 order_fill ='http://mapi.xinlv123.com/xltx/mobile/order';
+order_xq='http://mapi.xinlv123.com/xltx/mobile/order/details'
+
 
 
 
